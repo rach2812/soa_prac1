a2da374345600b21584dec5c06a9e42a401ea169
rach2812 <rquilligan@gmail.com>
2017-08-18 15:50:24 +1000

    Add files via upload

---
+++ prac1/prac1/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="prac1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    &nbsp;<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Calculate" />
+&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+&nbsp;&nbsp;
+        <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            <asp:ListItem>+</asp:ListItem>
+            <asp:ListItem>-</asp:ListItem>
+            <asp:ListItem>*</asp:ListItem>
+            <asp:ListItem>/</asp:ListItem>
+        </asp:DropDownList>
+&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+&nbsp;&nbsp;
+        <asp:Label ID="Label3" runat="server" Text="=Base10:"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server" OnTextChanged="TextBox3_TextChanged"></asp:TextBox>
+&nbsp;&nbsp;<asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Convert" />
+&nbsp;<asp:Label ID="Label4" runat="server" Text="=Base2:"></asp:Label>
+&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox6" runat="server" OnTextChanged="TextBox6_TextChanged"></asp:TextBox>
+        <br />
+        <br />
+&nbsp;<asp:Button ID="Button2" runat="server" Text="Count" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Num of 0"></asp:Label>
+&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Num of 1"></asp:Label>
+&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ prac1/prac1/WebForm1.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace prac1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double result = 0.0;
+            double num1 = Convert.ToDouble(TextBox1.Text);
+            double num2 = Convert.ToDouble(TextBox2.Text);
+
+            if (DropDownList1.Text.Equals("+"))
+            {
+                result = num1 + num2;
+                TextBox3.Text = result.ToString();
+            }
+            else if (DropDownList1.Text.Equals("-"))
+            {
+                result = num1 - num2;
+                TextBox3.Text = result.ToString();
+            }
+            else if (DropDownList1.Text.Equals("*"))
+            {
+                result = num1 * num2;
+                TextBox3.Text = result.ToString();
+            }
+            else if (DropDownList1.Text.Equals("/"))
+            {
+                result = num1 / num2;
+                TextBox3.Text = result.ToString();
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            /*double input = Convert.ToDouble(TextBox3.Text);
+            int whole = Convert.ToInt32(Math.Floor(input));
+            double dec = input - whole;
+            string binaryResult = "";
+            string binaryResult2 = "";
+            while (whole > 0)
+            {
+                binaryResult = (whole % 2).ToString() + binaryResult;
+                whole = whole / 2;
+            }
+            while (dec != 0)
+            {
+                dec = dec * 2;
+                if (dec > 1)
+                {
+                    binaryResult2 = binaryResult2 + "1";
+                    dec = dec - 1;
+                }
+            }
+            string finalResult = binaryResult + "." + binaryResult2;
+            TextBox6.Text = finalResult;*/
+        }
+        /* string base10Input = TextBox3.Text;
+         string binaryResult = "";
+         string binaryResult2 = "";
+         string[] splitInput = base10Input.Split('.');
+         int whole = Convert.ToInt32(splitInput[0]);
+         double dec = Convert.ToDouble(splitInput[1]);
+         if (dec == 0)
+         {
+             binaryResult = Convert.ToString(whole, 2);
+             TextBox6.Text = binaryResult;
+         }
+         else if (dec != 0)
+         {
+             while (whole > 0)
+             {
+                 binaryResult = whole % 2 + binaryResult;
+                 whole = whole / 2;
+             }
+             while (dec != 0)
+             {
+                 dec = dec * 2;
+                 if (dec > 1)
+                 {
+                     binaryResult2 = binaryResult2 + 1;
+                     dec = dec - 1;
+                 }
+             }
+             TextBox6.Text = binaryResult + "." + binaryResult2;
+         }*/
+
+        protected void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+            double input = Convert.ToDouble(TextBox3.Text);
+            int whole = Convert.ToInt32(Math.Floor(input));
+            double dec = input - whole;
+            string binaryResult = "";
+            string binaryResult2 = "";
+            while (whole > 0)
+            {
+                binaryResult = (whole % 2).ToString() + binaryResult;
+                whole = whole / 2;
+            }
+            while (dec != 0)
+            {
+                dec = dec * 2;
+                if (dec > 1)
+                {
+                    binaryResult2 = binaryResult2 + "1";
+                    dec = dec - 1;
+                }
+            }
+            string finalResult = binaryResult + "." + binaryResult2;
+            TextBox6.Text = finalResult;
+        }
+    }
+}
